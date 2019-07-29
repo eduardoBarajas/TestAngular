8820cbbe2b3a20506146bca307b367144323b93b
Eduardo <barajas.eduardo@uabc.edu.mx>
2019-07-28 18:53:41 -0700

    Comentarios agregados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,36 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  // Se declaran las 3 variables para las personas.
   persona1: Persona;
   persona2: Persona;
   persona3: Persona;
+  // formData es un objeto donde se almacenaran los datos del formulario para crear personas.
   formData = {edad: 0, nombre: '', sexo: 0, peso: 0, altura: 0};
+  // sexoMap es un mapa el cual se usa para el selector de sexos.
   sexoMap = new Map<number, string>();
+  // peronasChanged es un variable usada para activar la animacion cada que se crean las personas.
   personasChanged = false;
   title = 'Exam';
   ngOnInit(): void {
+    // se agregan los valores por defecto de hombre y mujer al mapa.
     this.sexoMap.set(0, 'Hombre');
     this.sexoMap.set(1, 'Mujer');
   }
 
   generarPersonas() {
+    // se validan los datos del formulario antes de crear a las personas.
     const valido = this.validarDatos();
     if (valido === null) {
+      // si los datos son validos entonces se activa la animacion y se crean las personas.
       this.personasChanged = false;
       setTimeout(e => {
+        // la primera persona debe tener todos los datos del formulario.
         this.persona1 = new Persona(this.formData.nombre, this.formData.edad, this.formData.sexo,
-        this.formData.peso, this.formData.altura);
+          this.formData.peso, this.formData.altura);
+        // la segunda debe tener todos menos los ultimos 2.
         this.persona2 = new Persona(this.formData.nombre, this.formData.edad, this.formData.sexo);
+        // la tercera es con el contructor por defecto y se agregan los campos con el setter.
         this.persona3 = new Persona();
         this.persona3.setAltura(1.76);
         this.persona3.setEdad(24);
@@ -37,12 +47,14 @@ export class AppComponent implements OnInit {
         this.personasChanged = true;
       }, 100);
     } else {
+      // si no es valido se muestra una alerta diciendo los fallos que tuvo.
       alert(valido);
     }
   }
 
   validarDatos(): string {
-    console.log(this.formData);
+    // se revisan los datos del formularo, si se encuentra un error en los datos se regresa inmediatamente la cadena con el error,
+    // si no hay errores se regresa un null.
     if (!isNaN(+this.formData.nombre)) {
       return 'Ingresa un nombre valido.';
     }
